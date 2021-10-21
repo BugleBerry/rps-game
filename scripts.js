748df734137dfeea7669ddd413927fb1f1cf7956
Nolan Tsai <nolanptsai@gmail.com>
2021-10-21 16:19:25 -0500

    Add UI to game

--- scripts.js
+++ scripts.js
@@ -19,65 +19,54 @@ function computerPlay() {
   }
 }
 
-function playRound() {
-  //prompt move from player, set to playerMove
-  let playerMove = prompt("What move would you like to play?");
-  //change playerMove to be lowercase
-  playerMove = playerMove.toLowerCase();
-  //notify player if playerMove is not any of the three valid moves
-  if (playerMove !== "rock" && playerMove !== "paper" && playerMove !== "scissors") {
-    return "Sorry, that is not a valid move. Please enter 'rock', 'paper' or 'scissors'";
-  }
+//weird but it doesn't make sense to declare scores in any of the functions
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerMove) {
   const computerMove = computerPlay();
   //if playerMove wins against computerMove return player win message
-  if ((playerMove === "rock" && computerMove === "scissors") || (playerMove === "paper" && computerMove === "rock") || (playerMove === "scissors" && computerMove === "paper")) {
-    return `You Win! (${playerMove} beats ${computerMove})`;
+  if ((playerMove === "rock" && computerMove === "scissors")
+      || (playerMove === "paper" && computerMove === "rock")
+      || (playerMove === "scissors" && computerMove === "paper")) {
+    playerScore++;
+    if (playerScore === 5) {
+      outputDisplay.textContent = `Good game! (${playerMove} beats ${computerMove})
+        Score is ${playerScore} : ${computerScore} Continue?`;
+    } else {
+      outputDisplay.textContent = `You Win! (${playerMove} beats ${computerMove})
+        Score is ${playerScore} : ${computerScore}`;
+    }
     //else if playerMove loses against computerMove return computer win message
-  } else if ((playerMove === "scissors" && computerMove === "rock") || (playerMove === "rock" && computerMove === "paper") || (playerMove === "paper" && computerMove === "scissors")) {
-    return `Computer Wins :( (${computerMove} beats ${playerMove})`;
-    //else return tie message
+  } else if ((playerMove === "scissors" && computerMove === "rock")
+      || (playerMove === "rock" && computerMove === "paper")
+      || (playerMove === "paper" && computerMove === "scissors")) {
+    computerScore++;
+    if (computerScore === 5) {
+      outputDisplay.textContent = `Good game! (${computerMove} beats ${playerMove})
+        Score is ${playerScore} : ${computerScore} Continue?`;
+    } else {
+      outputDisplay.textContent = `Computer Wins :( (${computerMove} beats ${playerMove})
+        Score is ${playerScore} : ${computerScore}`;
+    }
+      //else return tie message
   } else {
-    return "Tie!";
+    outputDisplay.textContent = `Tie! Both ${playerMove}!
+      Score is ${playerScore} : ${computerScore}`;
   }
 }
 
-function playMatch() {
-  //declare player and computer scores
-  let playerScore = 0;
-  let computerScore = 0;
-  //play game five times
-  for (let i = 0; i < 5; i++) {
-    let result = playRound();
-    //if round ends in player win, update and show scores
-    if (result.substr(0, 3) === "You") {
-      playerScore++;
-      console.log(`${result} score is ${playerScore} : ${computerScore}`);
-    }
-    //if round ends in computer win, update and show scores
-    if (result.substr(0, 3) === "Com") {
-      computerScore++;
-      console.log(`${result} score is ${playerScore} : ${computerScore}`);
-    }
-    //if round ends in tie, show scores
-    if (result.substr(0, 3) === "Tie") {
-      console.log(`${result} score is ${playerScore} : ${computerScore}`);
-    }
-    //if input is invalid, notify player and reduce index to allow for redo
-    if (result.substr(0, 3) === "Sor") {
-      console.log("Sorry, that is not a valid move. Please enter 'rock', 'paper' or 'scissors'");
-      i--;
-    }
-  }
-  //if playerScore is greater than computerScore, show victory message
-  if (playerScore > computerScore) {
-    return `You Win ${playerScore} : ${computerScore}!`;
-  }
-  //if computerScore is greater than playerScore, show loss message
-  if (playerScore < computerScore) {
-    return `You Lose ${playerScore} : ${computerScore} :(`;
-  }
-  //if scores are tied, show tie message
-  if (playerScore === computerScore) {
-    return `Tie! ${playerScore} : ${computerScore}`;
-  }
-}
+const rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener("click", () => {playRound('rock')});
+
+const paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener("click", () => {playRound('paper')});
+
+const scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener("click", () => {playRound('scissors')});
+
+const body = document.querySelector('body');
+const outputDisplay = document.createElement('div');
+outputDisplay.textContent = 'Choose a move to play! First to 5 wins!';
+body.appendChild(outputDisplay);
+
